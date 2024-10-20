7622f064e66f0f66c5aa70f0076a28a9953e0b1c
Lacy Morrow <me@lacymorrow.com>
2024-10-20 00:49:31 -0400

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { LandingPageComponent } from "@/components/views/landing-page";
-import { otelLogger } from "@/lib/otel-logger";
 
 const LandingPage = () => {
-  otelLogger.info('LandingPage');
-  console.log('LandingPage');
   return (
     <>
 
